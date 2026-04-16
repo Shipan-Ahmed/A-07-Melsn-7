100f2027d54050c1621b0fc92e9b7129fac7b5b7
Shipan Ahmed <shipanahmed1999@gmail.com>
2026-04-16 13:17:20 +0600

    updated navber

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -16,8 +16,17 @@ const Navber = () => {
         <nav className='bg-base-100 shadow-sm'>
             <div className="navbar  max-w-7xl mx-auto ">
                 <div className="navbar-start">
+                    
+                    <img src={logo} alt="" />
+                </div>
+                <div className='navbar-end'>
+                    <div className=" hidden md:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            {links};
+                        </ul>
+                    </div>
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost md:hidden ">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
                         <ul tabIndex="-1"
@@ -25,13 +34,7 @@ const Navber = () => {
                             {links};
                         </ul>
                     </div>
-                    <img src={logo} alt="" />
-                </div>
-                <div className="navbar-end hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {links};
-                    </ul>
-                </div>
+               </div>
                 
             </div>
         </nav>
--- src/Route/Root.jsx
+++ src/Route/Root.jsx
@@ -4,7 +4,7 @@ import HomePage from '../Pages/Homepages/HomePage';
 import MainLayout from '../Layouts/MainLayout';
 import TimeLine from '../Pages/TimeLine/TimeLine';
 import States from '../Pages/States/States';
-import Error from '../Components/Error';
+import Error from '../Components/Error/Error';
 
 const Root = createBrowserRouter([
     {
@@ -22,13 +22,11 @@ const Root = createBrowserRouter([
             {
                 path: "/States",
                 Component: States
-            },
-            {
-                path: "*",
-                Component: Error
             }
 
-        ]
+        ],
+        errorElement: <Error></Error>
+        
     }
 ])
 
