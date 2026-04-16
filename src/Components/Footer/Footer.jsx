5872fd908ebc52ebe9aaa16a3d57b24b6708fc35
Shipan Ahmed <shipanahmed1999@gmail.com>
2026-04-16 14:24:13 +0600

    added footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import footerImg from '../../assets/Images/logo-xl.png';
+import { FaFacebook, FaInstagram, FaInstagramSquare } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>this is footer section</h1>
-        </div>
+        <section className='bg-[#244D3F]'>
+            <div className='max-w-9/12 mx-auto pb-6 pt-10'>
+                <div>
+                    <div className='text-center space-y-4'>
+                        <img className='mx-auto' src={footerImg} alt="" />
+                        <p className='text-gray-400'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+                    </div>
+                    <div className='flex flex-col justify-center items-center mt-4 gap-2'>
+                        <h3 className='text-white'>Social Links</h3>
+                        <div className='flex gap-2'>
+                            <div className='p-2 bg-white rounded-2xl'><FaInstagram /></div>
+                            <div className='p-2 bg-white rounded-2xl'><FaFacebook /></div>
+                            <div className='p-2 bg-white rounded-2xl'><FaXTwitter /></div>
+                        </div>
+                       
+                    </div>
+                </div>
+                <div className='flex flex-col gap-4 md:flex-row text-gray-400 justify-between items-center border-t border-t-[#096446]  mt-8 pt-6'>
+                    <p>© 2026 KeenKeeper. All rights reserved.</p>
+                    <div className='flex gap-6'>
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service </p>
+                        <p>Cookies</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -14,17 +14,8 @@ const Navber = () => {
     </>
     return (
         <nav className='bg-base-100 shadow-sm'>
-            <div className="navbar  max-w-7xl mx-auto ">
+            <div className="navbar  max-w-11/12 mx-auto ">
                 <div className="navbar-start">
-                    
-                    <img src={logo} alt="" />
-                </div>
-                <div className='navbar-end'>
-                    <div className=" hidden md:flex">
-                        <ul className="menu menu-horizontal px-1">
-                            {links};
-                        </ul>
-                    </div>
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost md:hidden ">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
@@ -34,7 +25,13 @@ const Navber = () => {
                             {links};
                         </ul>
                     </div>
-               </div>
+                    <img src={logo} alt="" />
+                </div>
+                <div className="navbar-end hidden md:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {links};
+                    </ul>
+                </div>
                 
             </div>
         </nav>
